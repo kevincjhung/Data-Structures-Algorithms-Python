24867f1d67a0983f3bf45b2feb5a83d603b4e7f9
Kevin Hung <kevincjhung@gmail.com>
2024-05-16 19:02:59 -0700

    init sorting

---
+++ nodejs/03-sorting/sort.js
@@ -0,0 +1,12 @@
+let testArray = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 1, 2, 3, 4, 5, 6, 7]
+
+function selectionSort(arr){
+ 
+}
+
+
+function insertionSort(arr){
+
+}
+
+selectionSort(testArray)
