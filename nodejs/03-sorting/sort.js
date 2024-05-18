81e87121c5187155b77c10971584b72d9438c594
Kevin Hung <kevincjhung@gmail.com>
2024-05-18 08:51:54 -0700

    insertion sort js

--- nodejs/03-sorting/sort.js
+++ nodejs/03-sorting/sort.js
@@ -1,8 +1,6 @@
 let testArray = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 1, 2, 3, 4, 5, 6, 7]
 
 function selectionSort(arr){
-
-
   for(let i = 0; i < arr.length - 1; i++){
     let min_index = i; // index of the smallest element
 
@@ -16,6 +14,30 @@ function selectionSort(arr){
       [arr[i], arr[min_index]] = [arr[min_index], arr[i]];
     }
   }
+
+  return arr
 }
 
 
+
+function insertionSort(arr){
+  for(let i = 0; i < arr.length; i++){
+    // store the current element
+    let key = arr[i]
+
+    let j = i-1
+
+    while(j >= 0 && arr[j] > key){
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+
+    arr[j+1] = key
+  }
+
+  return arr
+}
+
+
+const sortedArray = insertionSort(testArray);
+console.log("Sorted Array:", sortedArray);
