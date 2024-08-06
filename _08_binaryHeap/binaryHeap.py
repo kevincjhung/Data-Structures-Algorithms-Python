f7c442aa780882b2a028028d8704036c0d15cf17
Kevin Hung <kevincjhung@gmail.com>
2024-08-05 17:25:36 -0700

    feature: BinaryHeap test suite progress

--- _08_binaryHeap/binaryHeap.py
+++ _08_binaryHeap/binaryHeap.py
@@ -8,4 +8,136 @@ class BinaryHeap:
         self.heap = []
         self.heap_type = heap_type  # 'min' for min-heap, 'max' for max-heap
 
-  
+    def _parent(self, index):
+        """
+        Return the index of the parent of the given index.
+
+        :param index: The index of the child.
+        :return: The index of the parent.
+        """
+        return (index - 1) // 2
+
+    def _left_child(self, index):
+        """
+        Return the index of the left child of the given index.
+
+        :param index: The index of the parent.
+        :return: The index of the left child.
+        """
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        """
+        Return the index of the right child of the given index.
+
+        :param index: The index of the parent.
+        :return: The index of the right child.
+        """
+        return 2 * index + 2
+
+    def _compare(self, child_value, parent_value):
+        """
+        Compare child and parent values based on heap type.
+
+        :param child_value: The value of the child node.
+        :param parent_value: The value of the parent node.
+        :return: True if the child and parent values should be swapped.
+        """
+        if self.heap_type == 'min':
+            return child_value < parent_value
+        elif self.heap_type == 'max':
+            return child_value > parent_value
+        else:
+            raise ValueError("heap_type must be 'min' or 'max'")
+
+    def _heapify_up(self, index):
+        """
+        Move the element at the given index up the heap to maintain the heap property.
+
+        :param index: The index of the element to move.
+        """
+        parent_index = self._parent(index)
+        
+        while index > 0 and self._compare(self.heap[index], self.heap[parent_index]):
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            index = parent_index
+            parent_index = self._parent(index)
+
+    def _heapify_down(self, index):
+        """
+        Move the element at the given index down the heap to maintain the heap property.
+
+        :param index: The index of the element to move.
+        """
+        size = len(self.heap)
+        
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+            smallest_or_largest = index
+            
+            if left_index < size and self._compare(self.heap[left_index], self.heap[smallest_or_largest]):
+                smallest_or_largest = left_index
+            if right_index < size and self._compare(self.heap[right_index], self.heap[smallest_or_largest]):
+                smallest_or_largest = right_index
+            
+            if smallest_or_largest != index:
+                self.heap[index], self.heap[smallest_or_largest] = self.heap[smallest_or_largest], self.heap[index]
+                index = smallest_or_largest
+            else:
+                break
+
+    def insert(self, value):
+        """
+        Insert a new value into the heap.
+
+        :param value: The value to insert.
+        """
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_root(self):
+        """
+        Remove and return the root value from the heap.
+
+        :return: The root value of the heap.
+        :raises IndexError: If the heap is empty.
+        """
+        if not self.heap:
+            raise IndexError("extract_root from an empty heap")
+
+        root_value = self.heap[0]
+        last_value = self.heap.pop()
+        if self.heap:
+            self.heap[0] = last_value
+            self._heapify_down(0)
+        
+        return root_value
+
+    def peek(self):
+        """
+        Return the root value without removing it.
+
+        :return: The root value of the heap.
+        :raises IndexError: If the heap is empty.
+        """
+        if not self.heap:
+            raise IndexError("peek from an empty heap")
+        return self.heap[0]
+
+    def __len__(self):
+        """
+        Return the number of elements in the heap.
+
+        :return: The number of elements in the heap.
+        """
+        return len(self.heap)
+
+    def __str__(self):
+        """
+        Return a string representation of the heap.
+
+        :return: String representation of the heap.
+        """
+        return str(self.heap)
+
--- tests/test_08_binaryHeap.py
+++ tests/test_08_binaryHeap.py
@@ -1,32 +1,92 @@
 import os
 import sys
 import unittest
+import pytest
+
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # from _07_avlTree.avlTree import AVLTree
 from _08_binaryHeap.binaryHeap import BinaryHeap
 
+    
+class TestHeapInitialization:
+    def test_default_initialization(self):
+        heap = BinaryHeap()
+        assert heap.heap_type == 'min'
+        assert len(heap) == 0
 
-def test_min_heap():
-    # Instantiate a min-heap
-    min_heap = BinaryHeap('min')
+    def test_custom_initialization(self):
+        min_heap = BinaryHeap('min')
+        max_heap = BinaryHeap('max')
+        assert min_heap.heap_type == 'min'
+        assert max_heap.heap_type == 'max'
+        assert len(min_heap) == 0
+        assert len(max_heap) == 0
+
+class TestHeapInsertion:
+    def test_single_insertion(self):
+        heap = BinaryHeap('min')
+        heap.insert(10)
+        assert heap.peek() == 10
+        assert len(heap) == 1
+
+    def test_multiple_insertion(self):
+        heap = BinaryHeap('min')
+        heap.insert(10)
+        heap.insert(5)
+        heap.insert(20)
+        assert heap.peek() == 5
+        assert len(heap) == 3
+
+    def test_heap_property(self):
+        min_heap = BinaryHeap('min')
+        min_heap.insert(10)
+        min_heap.insert(5)
+        min_heap.insert(20)
+        assert min_heap.peek() == 5
+        max_heap = BinaryHeap('max')
+        max_heap.insert(10)
+        max_heap.insert(5)
+        max_heap.insert(20)
+        assert max_heap.peek() == 20
+
+class TestHeapExtraction:
+    def test_extract_root(self):
+        heap = BinaryHeap('min')
+        heap.insert(10)
+        heap.insert(5)
+        heap.insert(20)
+        assert heap.extract_root() == 5
+        assert heap.peek() == 10
+        assert len(heap) == 2
+
+    def test_extract_multiple(self):
+        heap = BinaryHeap('max')
+        heap.insert(10)
+        heap.insert(20)
+        heap.insert(5)
+        assert heap.extract_root() == 20
+        assert heap.extract_root() == 10
+        assert heap.extract_root() == 5
+        assert len(heap) == 0
+
+    def test_extract_empty_heap(self):
+        heap = BinaryHeap('min')
+        with pytest.raises(IndexError):
+            heap.extract_root()
+
+class TestHeapPeek:
+    def test_peek_non_empty_heap(self):
+        heap = BinaryHeap('min')
+        heap.insert(5)
+        assert heap.peek() == 5
+
+    def test_peek_empty_heap(self):
+        heap = BinaryHeap('min')
+        with pytest.raises(IndexError):
+            heap.peek()
+
     
-    # Insert elements
-    min_heap.insert(10)
-    min_heap.insert(5)
-    min_heap.insert(3)
-    
-    # Check the root (should be the smallest element)
-    assert min_heap.peek() == 3
-    
-    # Extract root and check the new root
-    assert min_heap.extract_root() == 3
-    assert min_heap.peek() == 5
-    
-    # Extract remaining elements
-    assert min_heap.extract_root() == 5
-    assert min_heap.extract_root() == 10
-    
-    # Check if heap is empty
-    assert len(min_heap) == 0
+if __name__ == '__main__':
+    unittest.main()
